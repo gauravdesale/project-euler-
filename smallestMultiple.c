3c6b5214f5cd70aefee46628e2160d38de5a4fc0
gauravdesale <desalegaurav60@gmail.com>
2017-10-05 15:30:00 -0500

    this is the fifth problem

---
+++ smallestMultiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int smallestMultiple(int ,int);
+int main(){
+	int result = smallestMultiple(1, 20);
+	printf("%d\n", result);
+	return 0;
+}
+int smallestMultiple(int min, int max){
+	for(int i = 0; i < 100000; i++){
+		if((i%2 && i%3...) == 0){
+			return i;
+		}
+	}
+}
